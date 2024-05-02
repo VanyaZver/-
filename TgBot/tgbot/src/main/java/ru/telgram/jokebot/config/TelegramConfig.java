365015d5e08c414d7daf483cb3fcdadd740697ed
VanyaZver <161930734+VanyaZver@users.noreply.github.com>
2024-05-02 20:47:41 +0300

    Add files via upload

---
+++ TgBot/tgbot/src/main/java/ru/telgram/jokebot/config/TelegramConfig.java
@@ -0,0 +1,20 @@
+package ru.telgram.jokebot.config;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramConfig {
+
+    @Value("${telegram.bot.token}")
+    private String telegramToken;
+
+    @Bean
+    TelegramBot telegramBot() {
+        return new TelegramBot(telegramToken);
+
+
+    }
+}
